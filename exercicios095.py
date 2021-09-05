87041ea15456250ab74eeaa2025d19f487387dc9
João Luis B. Ribeiro <jl.berute@hotmail.com>
2021-09-05 18:13:29 -0300

    Update exercicios095.py

--- exercicios095.py
+++ exercicios095.py
@@ -1,14 +1,24 @@
 jogador = dict()
 
-jogador['Nome'] = str(input('digite o nome do jogador: ')).strip().capitalize()
-jogador['Jogos'] = int(str(input('digite a quantidade de jogos do jogador: ')).strip().capitalize())
+while True:
+    
+    jogador['Nome'] = str(input('digite o nome do jogador: ')).strip().capitalize()
+    jogador['Jogos'] = int(str(input('digite a quantidade de jogos do jogador: ')).strip().capitalize())
 
-tot = 0
+    tot = 0
 
-for c in range(0, jogador['Jogos']):
+    for c in range(0, jogador['Jogos']):
+        jogador[f'partida {c + 1}'] = int(input(f'quantos gols o jogador {jogador["Nome"]} fez na {c + 1}ª partida?'))
+        tot += jogador[f'partida {c + 1}']
 
-    jogador[f'partida {c+1}'] = int(input(f'quantos gols o jogador {jogador["Nome"]} fez na {c+1}ª partida?'))
-    tot += jogador[f'partida {c+1}']
+    while True:
+        r = str(input('deseja continuar? [S/N]')).strip().upper()
+
+        if r == 'S' or r == 'N':
+            break
+    if r == 'N':
+        break
+
 
 print(f'o jogador {jogador["Nome"]} jogou {jogador["Jogos"]} partidas.')
 
