b9308aa8ba9bb01f945146da071a43e26506fe20
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-06 17:33:32 -0300

    commit

--- exercicios104.py
+++ exercicios104.py
@@ -2,10 +2,11 @@ def leiaint(txt):
     while True:
         numero = input(txt)
         if numero.isnumeric() is True:
+            print('\033[32mTipo aceito com sucesso!\033[m')
             break
         else:
-            print("Tipo de dado incorreto, digite apenas Números, por favor! ")
+            print("\033[31mTipo de dado incorreto, digite apenas Números, por favor! \033[m")
     return print(numero)
 
 
-leiaint('Digite um valor numerico: ')
+leiaint('Digite um valor numérico: ')
