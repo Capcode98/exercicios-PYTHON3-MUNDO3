26f5a8b6ecff6d034c7908794a2f4ead010d8dd7
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-07 13:19:55 -0300

    commit

--- exercicios106.py
+++ exercicios106.py
@@ -1,16 +1,28 @@
 from time import sleep
 
 
+cores = {'normal': '\033[m',
+         'vermelho': '\033[m',
+         'verde': '\033[m',
+         'amarelo': '\033[m',
+         'azul': '\033[m',
+         'roxo': '\033[m',
+         'branco': '\033[m'}
+
+
 def PyHelp(fun):
 
-    txt = 'SISTEMA DE AJUDA PyHelp:'
-    print(f'^' * (len(txt) + 2))
-    print(f"SISTEMA DE AJUDA PyHelp:")
-    print(f'^' * (len(txt) + 2))
-    sleep(0.5)
     print('\033[1;33m')
     help(fun)
     print('\033[m')
+
+
+def titulo(txt='SISTEMA DE AJUDA PyHelp:', cor=0):
+
+    print(f'{cor}^' * (len(txt) + 4))
+    print(f"  {txt}")
+    print(f'{cor}^' * (len(txt) + 4))
+    sleep(0.5)
 
 
 PyHelp(input(":"))
