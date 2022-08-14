7c7f133b2c5bb492eea04f80e4fa15b5dd45780d
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-14 05:01:29 -0300

    commit

--- exercicios113_a_115C/funcoes_ex113/__init__.py
+++ exercicios113_a_115C/funcoes_ex113/__init__.py
@@ -9,7 +9,7 @@ def leiaint(txt):
             print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
 
         except:
-            print(f"\033[31mERRO {erro.__class__}: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
+            print(f"\033[31mERRO: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
 
         else:
             print('volte logo')
