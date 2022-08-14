f4255390e05fd533c3fb7c0950f286c939932e48
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-14 05:34:30 -0300

    commit

--- exercicios113.py
+++ exercicios113.py
@@ -1,3 +1,15 @@
 from exercicios113_a_115C import funcoes_ex113
 
-funcoes_ex113.leiaint(input('Digite um valor Inteiro: '))
+try:
+    funcoes_ex113.leiaint(input('Digite um valor Inteiro: '))
+
+except KeyboardInterrupt:
+    print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
+    print("Volte Sempre!")
+
+try:
+    funcoes_ex113.leiafloat(input('Digite um valor Racional: '))
+
+except KeyboardInterrupt:
+    print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
+    print("Volte Sempre!")
--- exercicios113_a_115C/funcoes_ex113/__init__.py
+++ exercicios113_a_115C/funcoes_ex113/__init__.py
@@ -5,18 +5,16 @@ def leiaint(txt):
         try:
             int(numero)
 
-        except KeyboardInterrupt:
-            print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
-            numero = input('Digite um valor Inteiro: ')
-
         except (TypeError, ValueError):
             print(f"\033[31mERRO: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
             numero = input('Digite um valor Inteiro: ')
 
         else:
             print('\033[32mNúmero aceito com sucesso!\033[m')
-            print('volte logo')
             return int(numero)
+
+        finally:
+            pass
 
 
 def leiafloat(txt):
@@ -26,15 +24,13 @@ def leiafloat(txt):
         try:
             float(numero)
 
-        except KeyboardInterrupt:
-            print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
-            numero = input('Digite um valor racional: ')
-
         except (TypeError, ValueError):
             print(f"\033[31mERRO: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
             numero = input('Digite um valor racional: ')
 
         else:
             print('\033[32mNúmero aceito com sucesso!\033[m')
-            print('volte logo')
             return float(numero)
+
+        finally:
+            pass
