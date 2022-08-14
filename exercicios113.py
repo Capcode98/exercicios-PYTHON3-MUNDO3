d8b7b241650343a835e1b7dbefa739fe9a28b398
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-14 05:19:52 -0300

    commit

---
+++ exercicios113.py
@@ -0,0 +1,3 @@
+from exercicios113_a_115C import funcoes_ex113
+
+funcoes_ex113.leiaint(input('Digite um valor Inteiro: '))
--- exercicios113_a_115C/funcoes_ex113/__init__.py
+++ exercicios113_a_115C/funcoes_ex113/__init__.py
@@ -7,15 +7,34 @@ def leiaint(txt):
 
         except KeyboardInterrupt:
             print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
+            numero = input('Digite um valor Inteiro: ')
 
-        except:
+        except (TypeError, ValueError):
             print(f"\033[31mERRO: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
+            numero = input('Digite um valor Inteiro: ')
 
         else:
+            print('\033[32mNúmero aceito com sucesso!\033[m')
             print('volte logo')
-            return numero
-            break
+            return int(numero)
 
 
+def leiafloat(txt):
+    numero = txt
 
+    while True:
+        try:
+            float(numero)
 
+        except KeyboardInterrupt:
+            print(f"\033[31mERRO: O usuario prefiriu não digitar um valor! \033[m")
+            numero = input('Digite um valor Inteiro: ')
+
+        except (TypeError, ValueError):
+            print(f"\033[31mERRO: Tipo de dado incorreto, digite apenas Números, por favor! \033[m")
+            numero = input('Digite um valor Inteiro: ')
+
+        else:
+            print('\033[32mNúmero aceito com sucesso!\033[m')
+            print('volte logo')
+            return float(numero)
