ecd235cdcfcd73fda13e32e8ccd2431be1eee09e
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-19 01:34:44 -0300

    commit

--- exercicios113_a_115C/arquivos_txt_ex115A/__init__.py
+++ exercicios113_a_115C/arquivos_txt_ex115A/__init__.py
@@ -1,26 +1,98 @@
-def arquivos(nome_do_arquivo, txt='', opcao=0):
+def AdicionarPessoas(nome_do_arquivo, txt=''):
 
     nome_do_arquivo = f'exercicios113_a_115C/arquivos_txt_ex115A/arquivos_de_texto/{nome_do_arquivo}'
 
-    if opcao == 0:
-        a = 'r'
-        with open(nome_do_arquivo, a) as arquivo:
+    intencao = "a"
 
-            texto_em_linhas = arquivo.readline()
-            for linha in texto_em_linhas:
-                print(linha)
+    try:
+        open(nome_do_arquivo, intencao)
 
-    elif opcao == 1:
-        a = 'w'
-        with open(nome_do_arquivo, a) as arquivo:
+    except Exception as error:
 
-            arquivo.write(txt)
-
-    elif opcao == 2:
-        a = 'a'
-        with open(nome_do_arquivo, a) as arquivo:
-
-            arquivo.write('\n' + txt)
+        return print(f"\033[31mERRO {error.__class__}: Arquivo não encontrado,"
+                     f" verifique e tente novamente, por favor! \033[m")
 
     else:
-        pass
+
+        with open(nome_do_arquivo, intencao) as arquivo:
+
+            texto = arquivo.seek(0, 0)
+
+            if texto != "":
+
+                arquivo.write(txt + '\n')
+
+            else:
+
+                arquivo.write(txt)
+
+        for linha in arquivo.readlines():
+
+            print(f'~~> {linha}', end='')
+        print()
+        return print(f'\033[32mArquivo encontrado e texto adicionado com sucesso!\033[m\nnome do arquivo: '
+                     f'{nome_do_arquivo}')
+
+    finally:
+        arquivo.close()
+
+
+def LeituraDePessoas(nome_do_arquivo):
+
+    nome_do_arquivo = f'exercicios113_a_115C/arquivos_txt_ex115A/arquivos_de_texto/{nome_do_arquivo}'
+
+    intencao = "r"
+
+    try:
+        open(nome_do_arquivo, intencao)
+
+    except Exception as error:
+
+        return print(f"\033[31mERRO {error.__class__}: Arquivo não encontrado,"
+                     f" verifique e tente novamente, por favor! \033[m")
+
+    else:
+
+        with open(nome_do_arquivo, intencao) as arquivo:
+
+            for linha in arquivo.readlines():
+
+                print(f'~~> {linha}', end='')
+
+            print()
+
+            return print(f'\033[32mArquivo encontrado e texto lido com sucesso!\033[m\nnome do arquivo: '
+                         f'{nome_do_arquivo}')
+
+    finally:
+
+        arquivo.close()
+
+
+def CriacaoDoArquivo(nome_do_arquivo, txt=''):
+
+    nome_do_arquivo = f'exercicios113_a_115C/arquivos_txt_ex115A/arquivos_de_texto/{nome_do_arquivo}'
+
+    intencao = "w"
+
+    try:
+
+        open(nome_do_arquivo, intencao)
+
+    except Exception as error:
+
+        return print(f"\033[31mERRO {error.__class__}: Arquivo não encontrado,"
+                     f" verifique e tente novamente, por favor! \033[m")
+
+    else:
+
+        with open(nome_do_arquivo, intencao) as arquivo:
+
+            arquivo.write(txt.upper() + '\n')
+
+            return print(f'\033[32mArquivo criado e Titulo adicionado com sucesso!\033[m\nnome do arquivo: '
+                         f'{nome_do_arquivo}')
+
+    finally:
+
+        arquivo.close()
--- exercicios115A.py
+++ exercicios115A.py
@@ -1,8 +1,14 @@
-from exercicios113_a_115C.arquivos_txt_ex115A import arquivos
+from exercicios113_a_115C import arquivos_txt_ex115A
 
 nome_do_arquivo = input('Digite o nome do arquivo: ')
-opcao = int(input('Qual a opção : '))
-txt = input('Digite o texto a ser adcionado: ')
 
-arquivos(nome_do_arquivo, txt, opcao)
-arquivos(nome_do_arquivo)
+txt = input('Digite o texto a ser adcionado: ')
+try:
+    open(f'exercicios113_a_115C/arquivos_txt_ex115A/arquivos_de_texto/{nome_do_arquivo}')
+except FileNotFoundError:
+    print(f'\033[31mArquivo: {nome_do_arquivo} está sendo criado agora!\033[m')
+else:
+    print('f\033[32mArquivo já está criado:\033[m')
+
+arquivos_txt_ex115A.CriacaoDoArquivo(nome_do_arquivo, txt)
+
