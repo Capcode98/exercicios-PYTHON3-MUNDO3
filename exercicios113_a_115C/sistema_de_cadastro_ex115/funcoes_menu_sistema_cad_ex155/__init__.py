4afcf9f4aab85ae4b7c75690280d19fb40e7bd01
João Luis B. Ribeiro <89806860+Capcode98@users.noreply.github.com>
2022-08-22 05:49:07 -0300

    commit

--- exercicios113_a_115C/sistema_de_cadastro_ex115/funcoes_menu_sistema_cad_ex155/__init__.py
+++ exercicios113_a_115C/sistema_de_cadastro_ex115/funcoes_menu_sistema_cad_ex155/__init__.py
@@ -5,10 +5,19 @@ def Menu():
     print('-' * 45)
     print(f'{"MENU PRINCIPAL":^45}')
     print('-' * 45)
+    print(f'\033[35m 1 :\033[m\033[32m ~Ver pessoas cadastradas!\033[m')
+    print(f'\033[35m 2 :\033[m\033[33m ~Cadastrar uma nova pessoa!\033[m')
+    print(f'\033[35m 3 :\033[m\033[34m ~Sair do Sistema!\033[m')
 
 
-def ContagemRegressiva(s):
-    s = 0
-    while s != 5:
+def ContagemRegressiva():
+    print('Contagem Regressiva Iniciada! ')
+    for i in range(5, -1, -1):
         sleep(1)
-        print(f'Contagem Regressiva Iniciada!\n {s} segundos!')
+        print(f'Contagem Regressiva: {i}')
+    return 0
+
+
+def PegarAOpcao():
+    p = int(input('Digite o numero correspondente a sua opção: '))
+    return p
--- exercicios115.py
+++ exercicios115.py
@@ -1,14 +1,22 @@
-from exercicios113_a_115C import sistema_de_cadastro_ex115
+from exercicios113_a_115C.sistema_de_cadastro_ex115.funcoes_sistema_cad_ex155 import CriacaoDoArquivo, \
+    AdicionarPessoas, DeletarArquivo, LeituraDePessoas
 
 nome_do_arquivo = input('Digite o nome do arquivo: ')
 
 txt = input('Digite o texto a ser adcionado: ')
+
 try:
-    open(f'exercicios113_a_115C/sistema_de_cadastro_ex115/arquivos_de_texto/{nome_do_arquivo}')
+
+    open(f'exercicios113_a_115C/sistema_de_cadastro_ex115/funcoes_sistema_cad_ex155/arquivos_de_texto/'
+         f'{nome_do_arquivo}')
+
 except FileNotFoundError:
+
     print(f'\033[31mArquivo: {nome_do_arquivo} está sendo criado agora!\033[m')
+
 else:
+
     print('f\033[32mArquivo já está criado:\033[m')
 
-sistema_de_cadastro_ex115.CriacaoDoArquivo(nome_do_arquivo, txt)
+CriacaoDoArquivo(nome_do_arquivo, txt)
 
